435b340d2e0e1ff62e29337da1cd9d611aac3f8a
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-07-23 21:15:20 +0530

    blog

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 
 	"github.com/lariv-in/lariv"
+	"github.com/lariv-in/lariv/plugins/p_blog"
 	"github.com/lariv-in/lariv/plugins/p_dashboard"
 	"github.com/lariv-in/lariv/plugins/p_filesystem"
 	"github.com/lariv-in/lariv/plugins/p_no_signup"
@@ -19,6 +20,7 @@ func main() {
 		p_users.GetPlugin(),
 		p_website.GetPlugin(),
 		p_no_signup.GetPlugin(),
+		p_blog.GetPlugin(),
 	}
 
 	config, err := lariv.LoadConfigFromFile("config.toml", plugins)
